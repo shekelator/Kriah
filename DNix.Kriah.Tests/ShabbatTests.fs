b1e9a3414474f8dd0cbc0d7069316f4c72851bba
Dave Nichol <shekelator@gmail.com>
2021-06-20 17:14:06 -0400

    Pull in other tests. Needs some work!

---
+++ DNix.Kriah.Tests/ShabbatTests.fs
@@ -0,0 +1,18 @@
+﻿module ShabbatTests
+
+open System
+open System.IO
+open NUnit.Framework
+open FsUnit
+open DNix.Kriah.Core
+
+[<Test>]
+let ``Gets items`` () =
+    let data = File.ReadAllText("./hebcal-data.json")
+    let result = Parshiot.Parse(data)
+    let parasha = Seq.find (fun (x: Parshiot.Reading) -> x.Title = "Shemot") result
+    let shabbat = Shabbat.parashaToShabbat parasha
+    shabbat.Date |> should equal (new DateTime(2018, 1, 6))
+    shabbat.Title |> should equal "Shemot"
+    shabbat.Id.IsNone |> should be True
+
