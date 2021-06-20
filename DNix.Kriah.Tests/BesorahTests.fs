a20555c6982e0631ba22845df073800ae95957fb
Dave Nichol <shekelator@gmail.com>
2021-06-20 16:26:13 -0400

    Add FsUnit, fix tests

--- DNix.Kriah.Tests/BesorahTests.fs
+++ DNix.Kriah.Tests/BesorahTests.fs
@@ -4,6 +4,7 @@ open System
 open System.IO
 open NUnit.Framework
 open DNix.Kriah.Core
+open FsUnit
 // open DNix.Kriah.Core.Besorot
 
 let GetCycle cycleString = 
@@ -22,7 +23,7 @@ let Setup () =
 [<TestCase(5782)>]
 let ``Bereshit always has the beginning of John`` (year) =
     let result = Besorot.GetReadingBySedra year "Bereshit" None
-    Assert.Equals("John 1:1-18", Option.get(result).Reading)
+    "John 1:1-18" |> should equal (Option.get(result).Reading)
 
 [<TestCase(5777, "A")>]
 [<TestCase(5778, "B")>]
@@ -37,19 +38,19 @@ let ``Chooses correct cycle based on year`` (year: int, cycleString: string) =
         | _ -> Besorot.Year.C
 
     let readingYear = Besorot.GetYear year
-    Assert.Equals(cycle, readingYear)
+    cycle |> should equal readingYear
 
 
 [<TestCase("Yitro", 5782, "John 7:1-13")>]
 [<TestCase("Vayechi", 5781, "Luke 5:27-39")>]
 let ``Can get a few normals ones right`` (sedra: string, year: int, expected: string) =
     let result = Besorot.GetReadingBySedra year sedra None
-    Assert.Equals(expected, Option.get(result).Reading)
+    expected |> should equal (Option.get(result).Reading)
 
 [<TestCase("Mishpatim", 5782)>]
 [<TestCase("Vayakhel", 5782)>]
 let ``Can get shabbat shekalim right`` (sedra: string, year: int) =
     let expected = "Mark 12:41-44"
     let result = Besorot.GetReading year {Sedra = sedra; Shabbat = Some Besorot.SpecialShabbat.Shekalim}
-    Assert.Equals(expected, result.Value.Reading)
+    result.Value.Reading |> should equal expected
 
